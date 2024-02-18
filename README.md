5f4c545b7096ac802c30068db3dbe9aae83c4737
kedicode <kedicode@gmail.com>
2024-02-17 22:13:46 -0500

    Add message and work on moving. Might have to use streams.

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
  Not a ton here yet.
 ## How to run
 Npm install and then call node app.js with the total minutes you want
-
+and the message as a string
 ``` javascript
-node app.js 5
+node app.js 5 "message"
 ```
 
 
 ## In Progress
 - [x] Find node modules to use
 - [x] Convert the input into total seconds
+- [x] Add message param
 - [ ] Convert show the input in countdown format with m:ss left
--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const figlet = require("figlet");
+const tty = require("node:tty");
 
 function app() {
     //  capture the time that is entered in to kstah
     const countdown = process.argv[2];
+    const message = process.argv[3];
     console.log(countdown);
     let totalInterval = parseInt(countdown * 60);
+    // get our process
+    const ttyProcess = process.stdout;
     const running = setInterval(() => {
+        // ttyProcess.cursorTo(0, 0, () => {
+        //     console.clear();
+        // });
         console.clear();
-        figlet(totalInterval--, (err, data) => {
+        // const [width, height] = ttyProcess.getWindowSize();
+        // const x = Math.floor(Math.random() * width);
+        // const y = Math.floor(Math.random() * height);
+        // ttyProcess.cursorTo(x, y);
+        figlet(`${message}: ${totalInterval--}`, (err, data) => {
             console.log(data);
         });
-
+        // console.log(width, height);
         if(totalInterval < 0){
             clearInterval(running);
         }
